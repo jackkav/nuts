6b3623ba0319069bb4984453a0a6ccbf60dd2e7e
Jack Kavanagh <jackkav@gmail.com>
2017-02-27 19:34:17 +0800

    made a simple seach and results layout

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  Text, TextInput,
+  View, ScrollView,
 } from 'react-native';
 class Blink extends Component {
   constructor(props) {
     super(props);
-    this.state = {showText: true};
+    this.state = { showText: true };
 
     // Toggle the state every second
     setInterval(() => {
@@ -29,19 +29,80 @@ class Blink extends Component {
     );
   }
 }
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render() {
+    return (
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+        <Text>My name is</Text>
+      </View>
+    );
+  }
+}
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={{ flex: 0.1, backgroundColor: 'powderblue' }}>
+          <Blink style={styles.welcome} text='Search Bar' />
+        </View>
+    );
+  }
+}
 export default class nuts extends Component {
   render() {
     return (
-      <View style={{flex:1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+      <View style={{ flex: 1 }}>
+        <SearchBar />
+        <ScrollView style={{ flex: 1, backgroundColor: 'skyblue' }}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </ScrollView>
+        {/*<View style={{flex: 1, backgroundColor: 'powderblue'}}>
           <Blink style={styles.welcome} text='Welcome to React Native!' />
         </View>
+        <ScrollView>
         <View style={{flex: 2, backgroundColor: 'skyblue'}} >
-          <Blink style={styles.welcome} text='To get started, edit index.android.js' />
+          <Blink style={styles.welcome} text='To get started, type below' />
         </View>
         <View style={{flex: 3, backgroundColor: 'steelblue'}} >
-          <Blink style={styles.welcome} text='Double tap R on your keyboard to reload' />
+          <PizzaTranslator style={styles.welcome} />
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
         </View>
+        </ScrollView>*/}
       </View>
     );
   }
